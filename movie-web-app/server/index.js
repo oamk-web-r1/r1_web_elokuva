8ddd0adf5dae29db4b6e919dc0f14e91cae2b9d1
Tatu Lehtola <tlehtola@students.oamk.fi>
2024-11-12 11:13:07 +0200

    app.post
    app.post function done. It doesnt do anything yet.

--- movie-web-app/server/index.js
+++ movie-web-app/server/index.js
@@ -1,31 +1,26 @@
-<<<<<<< Updated upstream
-const express = require('express');
-const path = require('path');
-
-const app = express();
-const PORT = 3001;
-=======
 import express from 'express';
 import cors from 'cors';
 import pkg from 'pg';
-import { Router } from 'express';
 
 const PORT = 3001;
 const { Pool } = pkg;
 
 const app = express();
 app.use(cors());
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
 
->>>>>>> Stashed changes
 
 app.get('/', (req, res) => {
-  res.json('Hello from the backend!');
+
+  const pool = openDb()
+
+  pool.query('select * from Users', (error, result) => {
+    if (error) {
+      return res.status(500).json({error: error.message});
+    }
+    res.status(200).json(result.rows);
+  })
 });
 
-<<<<<<< Updated upstream
-=======
 app.post('/create',(req,res) => {
   pool.query('insert into Users (id, email, password_hash) values ($1, $2, $3) returning *'),
   [req.body.id],
@@ -55,7 +50,6 @@ const openDb = () => {
 
 
 
->>>>>>> Stashed changes
 app.listen(PORT, () => {
-  console.log('Server is running');
-});
+  console.log(`Server is running on http://localhost:${PORT}`)
+})
