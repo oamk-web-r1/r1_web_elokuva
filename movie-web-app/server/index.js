332dddb2906d73c998b3e173116b80d43265ac69
himavirveli <158284297+himavirveli@users.noreply.github.com>
2024-11-12 10:14:42 +0200

    Backend/db  setup
    Connection to local Postqres DB, replaced "require" in index with ES6-modules.

--- movie-web-app/server/index.js
+++ movie-web-app/server/index.js
@@ -1,12 +1,40 @@
-const express = require('express');
-const path = require('path');
+import express from 'express';
+import cors from 'cors';
+import pkg from 'pg';
+
+const PORT = 3001;
+const { Pool } = pkg;
 
 const app = express();
-const PORT = 3001;
+app.use(cors());
+
 
 app.get('/', (req, res) => {
-  res.json('Hello from the backend!');
+  const pool = openDb()
+
+  pool.query('select * from Users', (error, result) => {
+    if (error) {
+      return res.status(500).json({error: error.message});
+    }
+    res.status(200).json(result.rows);
+  })
 });
+
+const openDb = () => {
+  const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'Web_sovellus_DB_template1',
+    password: 'root',
+    port: 5432
+  })
+  return pool
+}
+
+
+
+
+
 
 app.listen(PORT, () => {
   console.log('Server is running');
