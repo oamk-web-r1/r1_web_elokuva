7f151a7e9c679768c1c2c28e7bf33488007d0b4e
KarolusPaulus <karosaarinenn@gmail.com>
2024-11-18 13:00:15 +0200

    small update

--- movie-web-app/src/components/movie.js
+++ movie-web-app/src/components/movie.js
@@ -6,39 +6,24 @@ const MyKey = process.env.REACT_APP_API_KEY
 
 function Movie({ movies }) {
   const [movieList, setMovieList] = useState([]);
-  const [currentPage, setCurrentPage] = useState(1);
-  const [totalPages, setTotalPages] = useState(1);
 
-  const getMovies = (page) => {
-    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${MyKey}&language=en-US&page=${page}`)
+  const getMovies = () => {
+    fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${MyKey}&language=en-US&page=1`)
       .then(res => res.json())
       .then(json => {
         setMovieList(json.results)
-        setTotalPages(json.total_pages)
       })
       .catch(err => console.error(err))
   };
 
   useEffect(() => {
     if (movies.length === 0) {
-      getMovies(currentPage)
+      getMovies()
     }
       else {
         setMovieList(movies)
       }
-  }, [currentPage, movies])
-
-  const handleNextPage = () => {
-    if (currentPage < totalPages) {
-      setCurrentPage(currentPage + 1)
-    }
-  }
-
-  const handlePrevPage = () => {
-    if (currentPage > 1) {
-      setCurrentPage(currentPage - 1);
-    }
-  }
+  }, [movies])
 
   return (
     <>
@@ -46,25 +31,17 @@ function Movie({ movies }) {
       {
       movieList.map((movie) => (
         <div class="movie-card" key={movie.id}>
-          <Link to={`/moviepage/${movie.id}`}> 
+          <Link to={`/moviepage`}> 
           <img class="poster-image" src={`https://image.tmdb.org/t/p/w500${movie.poster_path}`} alt={movie.title} />
           </Link>
           <p class="movie-title">{movie.title}</p>
         </div>
       ))}
     </div>
-    <div className="pagination-controls">
-        <button onClick={handlePrevPage} disabled={currentPage === 1}>
-          Previous
-        </button>
-        <span>Page {currentPage} of {totalPages}</span>
-        <button onClick={handleNextPage} disabled={currentPage === totalPages}>
-          Next
-        </button>
-      </div>
     </>
   )
 }
 
 export default Movie
+
 
