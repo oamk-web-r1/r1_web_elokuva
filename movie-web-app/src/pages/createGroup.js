f2bd344c03932664fc047b9ab577841a30e2340e
himavirveli <158284297+himavirveli@users.noreply.github.com>
2024-11-29 13:46:56 +0200

    Create Group page
    createGroup.sj
    
    - Functional form to create a new group
    - Redirect to allgroups after successfull create
    
    Tested and groups show in DB as should.

--- movie-web-app/src/pages/createGroup.js
+++ movie-web-app/src/pages/createGroup.js
@@ -1,11 +1,78 @@
-
-import React from "react";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { useUser } from '../context/useUser'; 
+import Header from '../components/header';
 
 export function CreateGroup() {
+    const navigate = useNavigate();
+    const { user } = useUser();
+    const [groupData, setGroupData] = useState({
+        name: '',
+        description: ''
+    });
+    const [error, setError] = useState(null);
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+        try {
+            const response = await fetch('http://localhost:3001/groups/create', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify({
+                    owner_id: user.user_id,
+                    name: groupData.name,
+                    description: groupData.description
+                })
+            });
+
+            const data = await response.json();
+            
+            if (!response.ok) {
+                throw new Error(data.error || 'Failed to create group');
+            }
+
+            //Show success alert
+            alert('Group created successfully! You can view your groups at Groups page.');
+
+            // Redirect to groups page on success
+            navigate('/allgroups');
+        } catch (error) {
+            setError(error.message);
+        }
+    };
+    
     return (
+    <>
+        <Header />
         <div>
-            <h3 >Create Group</h3>
+            <h3 className="default-big-title-pink">Create a group</h3>
+            <div className="gray-box">
+                {error && <div className="error-message">{error}</div>}
+                <form onSubmit={handleSubmit}>
+                    <div>
+                        <label className="default-text">Group name</label>
+                        <input
+                            type="text"
+                            value={groupData.name}
+                            onChange={e => setGroupData({ ...groupData, name: e.target.value })}
+                            required
+                        />
+                    </div>
+                    <div>
+                        <label className="default-text">Description</label>
+                        <input
+                            type="text"
+                            value={groupData.description}
+                            onChange={e => setGroupData({ ...groupData, description: e.target.value })}
+                            required
+                        />
+                    </div>
+                    <button type="submit" className="default-button">Publish Group</button>
+                </form>
+            </div>
         </div>
+    </>
     );
 }
-    
